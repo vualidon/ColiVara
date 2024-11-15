57a8ba74cd87049440a3dabe84ddcd94a0c2c275
Abdullah Nassar <abdullah.u.nassar@vanderbilt.edu>
2024-11-15 16:01:29 +0200

    remove any provided extension

--- web/api/models.py
+++ web/api/models.py
@@ -143,11 +143,11 @@ class Document(models.Model):
             # Generate filename
             safe_name = self.name.replace(" ", "_")
 
-            # Ensure the extension is not duplicated
-            if safe_name.endswith(extension):
-                filename = safe_name
-            else:
-                filename = f"{safe_name}{extension}"
+            # Remove any existing extension (if present)
+            name_without_extension, _ = os.path.splitext(safe_name)
+
+            # Append the correct extension
+            filename = f"{name_without_extension}{extension}"
 
             # Save to S3
             await sync_to_async(self.s3_file.save)(
